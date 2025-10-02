d7d28d7b047e8c9c6e8964aebb7b689a3c9bd1d4
mgomez3Nora <mgomez3@murraystate.edu>
2025-10-01 19:16:27 -0500

    Update index.js
    more updates to bugs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import fetch from "node-fetch";
 import cors from "cors";
 import admin from "firebase-admin";
-import serviceAccount from "./serviceAccountKey.json";  // ✅ fixed import
+import serviceAccount from "/etc/secrets/firebase-key.json" assert { type: "json" };  // ✅ fixed import
 
 const app = express();
 app.use(cors());
